d197ab74c70279a210678c4f2cfb1159e46697d9
Marcus Fernando Correa Barbosa <marcus.barbosa.ext@rendimento.com.br>
2019-08-19 17:11:28 -0300

    criando os repositorios o contexto do Banking

---
+++ MicroRabbit/MicroRabbit.Banking.Data/Repository/AccountRepository.cs
@@ -0,0 +1,23 @@
+﻿using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Domain._1_Models;
+using MicroRabbit.Banking.Domain._2_Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Banking.Data.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private BankingDbContext _context;
+
+        public AccountRepository(BankingDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Account> GetAccounts()
+        {
+            return _context.Accounts;
+        }
+    }
+}
---
+++ MicroRabbit/MicroRabbit.Banking.Domain/2-Interfaces/IAccountRepository.cs
@@ -0,0 +1,13 @@
+﻿using MicroRabbit.Banking.Domain._1_Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Banking.Domain._2_Interfaces
+{
+    public interface IAccountRepository
+    {
+        IEnumerable<Account> GetAccounts();
+
+    }
+}
